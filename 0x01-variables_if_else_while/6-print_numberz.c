043314c897bdb6f81318b2f3c02cb38744f5056e
farah-halim <level2bis2@gmail.com>
2023-03-17 12:30:39 -0700

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: print number 0 to 9
+ * Return: 0 (success)
+ */
+
+int main (void)
+{
+	int x;
+	for (x=1 ; x<=10 ;x++ )
+		putchar(x + 48 );
+	putchar ("\n");
+	return (0);
+	
+
+
+}
