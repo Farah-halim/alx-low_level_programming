38d0ffa19ebbd45ff3d16990daabf7068ca806a9
Farah-halim <level2bis2@gmail.com>
2023-04-28 06:46:47 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,14 @@
 #include "lists.h"
 
+void _constructor(void) __attribute__ ((constructor));
+
 /**
- * first - executes a function before main
+ * _constructor - executes before main()
  *
+ * Return: void
  */
-void __attribute__((__constructor__))first(void)
+void _constructor(void)
 {
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+		"I bore my house upon my back!\n");
 }
