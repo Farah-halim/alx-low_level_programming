67e377bedaa17533d01e8a932df39732f003f635
Farah-halim <level2bis2@gmail.com>
2023-04-29 14:00:57 -0700

    done:

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,11 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-/*
- * Desc: Header file containing prototypes and definitions for all functions
- *       and types written in the 0x12-more_singly_linked_lists directory.
- */
-
 #include <stdlib.h>
 
 /**
@@ -14,8 +9,9 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for Holberton project
+ * for project
  */
+
 typedef struct listint_s
 {
 	int n;
