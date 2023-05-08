f717542f74c1de0d1cb8b5fdaf8e1c4478f39e2b
Farah-halim <level2bis2@gmail.com>
2023-05-08 11:38:58 -0700

    task1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,14 +16,14 @@ int create_file(const char *filename, char *text_content)
 	if (file == -1)
 		return (-1);
 
-	if (text_content)
+	if (text_content != NULL)
 	{
 		while (text_content[i])
 			i++;
-		w = write(file, text_content, i);
-		if (w == -1)
-			return (-1);
 	}
+	w = write(file, text_content, i);
+	if (w == -1)
+		return (-1);
 	close(file);
 	return (1);
 }
