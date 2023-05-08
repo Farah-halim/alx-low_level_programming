d9b7c9a8f7aead093f18b8bd51f277b280bb3713
Farah-halim <level2bis2@gmail.com>
2023-05-08 15:39:24 -0700

    test

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,10 +7,8 @@
  */
 int main(int argc, char *argv[])
 {
-	int file_from;
-	int file_to;
-	ssize_t re;
-	ssize_t wr;
+	int file_from, file_to;
+	ssize_t re = 1024, wr, close_file;
 	char buffer[1024];
 
 	if (argc != 3)
@@ -24,35 +22,35 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	while ((re = read(file_from, buffer, 1024)) != 0)
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (file_to == -1)
 	{
-		if (file_to == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
-		}
-		if (read_len == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	while (re == 1024)
+	{
+		re = read(file_from, buffer, 1024);
+		if (re == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			exit(98);
 		}
 		wr = write(file_to, buffer, re);
-		if (wr != re || wr == -1)
+		if (wr == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
 	}
-	if (close(file_from) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
-		exit(100);
-	}
-	if (close(file_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
-		exit(100);
-	}
-	return (0);
+
+	close_file = close(file_from);
+	if (close_file == -1)
+	{ dprintf(STDERR_FILENO, "Error: Can't close fd  %d\n", file_from);
+		exit(100); }
+	close_file = close(file_to);
+	if (close_file == -1)
+	{ dprintf(STDERR_FILENO, "Error: Can't close fd  %d\n", file_to);
+		exit(100); }
+return (0);
 }
