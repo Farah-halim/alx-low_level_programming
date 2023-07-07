1cde934b183a2b0456d91947957f8d237dcb92c4
Farah-Halim <level2bis2@gmail.com>
2023-07-07 09:41:05 -0700

    task6

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -5,21 +5,22 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
-	hash_node_t *next, *ptr;
 	unsigned long int i;
+	hash_node_t *tmp1;
+	hash_node_t *tmp2;
 
 	if (ht == NULL)
 		return;
+
 	for (i = 0; i < ht->size; i++)
 	{
-		ptr = ht->array[i];
-		while (ptr)
+		tmp1 = ht->array[i];
+		while ((tmp2 = tmp1) != NULL)
 		{
-			next = ptr->next;
-			free(ptr->key);
-			free(ptr->value);
-			free(ptr);
-			ptr = next;
+			tmp1 = tmp1->next;
+			free(tmp2->key);
+			free(tmp2->value);
+			free(tmp2);
 		}
 	}
 	free(ht->array);
